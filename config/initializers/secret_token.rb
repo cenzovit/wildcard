828b796ebfb14f70e548ead0e5ccb5a2867c5079
Vincenzo Vitiello <cenzo.vit@gmail.com>
2014-03-07 18:48:52 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wildcard::Application.config.secret_key_base = 'dcf1985ead674e9f2970019a533d997a2025c57bf05e9d3dae055906034da0fa5f33e9eedaf1e118316e8b8caee6425aa39ad98738955f763157edfdebaab496'
